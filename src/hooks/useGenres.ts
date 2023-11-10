7b660b29629fb1ad4879fbdeaceeb4e3b3155780
giacomocolturri <colturri@gmail.com>
2023-11-10 11:23:33 +1000

    Simplify time calculation with ms library

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-client";
+import ms from "ms";
 import genres from "../data/genres";
+import APIClient from "../services/api-client";
 
 export interface Genre {
   id: number;
@@ -14,7 +15,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24hours
+    staleTime: ms("24h"),
     initialData: genres,
     /*initialData: {
       count: genres.length,
